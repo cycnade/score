bdeab6c4626603c444f0c5c3a9a06ba5db2f45c3
Faraz Khawaja <59088548+cycnade@users.noreply.github.com>
2020-04-21 18:03:28 +0500

    Update score.js

--- score.js
+++ score.js
@@ -2,30 +2,48 @@
 var num=prompt("how many num of games")
 var para=document.getElementById("games")
 para.textContent="Playing to: "+num
+var sone=document.getElementById("d1")
+var stwo=document.getElementById("d2")
 var pone=document.getElementById("one")
 var ptwo=document.getElementById("two")
 var rst=document.getElementById("reset")
 var head = document.querySelector("h1")
 rst.addEventListener("click",function(){
-	location.reload()
+	 num=prompt("how many num of games")
+	para.textContent="Playing to: "+num
+	p1=0
+	sone.innerHTML=p1
+	p2=0
+	sone.classList.remove("win")
+	stwo.classList.remove("win")
+	stwo.innerHTML=p2
+	pone.style.background="red"
+	
 })
+
+
 p1=0
 p2=0
 pone.style.background="red"
-head.textContent=p1+" To "+p2
 pone.addEventListener("click",function(){
 	if (p1<num && p2!=num){
 		p1=p1+1
-	head.textContent=p1+" To "+p2
-}
+	sone.innerHTML=p1
+	if (p1==num){
+		sone.classList.add("win")
+	}
+
+}	
 })
 	
 	
 ptwo.addEventListener("click",function(){
 	if (p2<num && p1!=num){
 	p2=p2+1
-	head.textContent=p1+" To "+p2
-
+	stwo.innerHTML=p2
+	}
+	if (p2==num){
+		stwo.classList.add("win")
 	}
 })
 
